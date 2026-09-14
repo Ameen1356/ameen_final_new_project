104dc4b21a79937de521f1c021a7c82cb061ce85
class01 <ameensholy987@gmail.com>
2026-09-14 10:44:29 +0300

    wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww

---
+++ app/src/main/java/am/sh/ameen_final_new_project/data/MySubjectTable/MySubjectQuery.java
@@ -0,0 +1,4 @@
+package am.sh.ameen_final_new_project.data.MySubjectTable;
+
+public interface MySubjectQuery {
+}
---
+++ app/src/main/java/am/sh/ameen_final_new_project/data/MyTaskTable/MyTask.java
@@ -0,0 +1,4 @@
+package am.sh.ameen_final_new_project.data.MyTaskTable;
+
+public class MyTask {
+}
